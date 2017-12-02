50ccf909ddeed5e42bcb075b876131c85ec18601
Adrian Cucho <adriancucho@gmail.com>
2017-12-02 18:58:16 -0500

    desin fragment
    Se aplico diseno

--- app/src/main/java/com/armaelpack/armaelpack_envios/PantallaPrincipal.java
+++ app/src/main/java/com/armaelpack/armaelpack_envios/PantallaPrincipal.java
@@ -79,6 +79,8 @@ public class PantallaPrincipal extends AppCompatActivity
     /**Se terminan los metodos**/
 
 
+
+    /** METODOS DENTRO DEL VIEWDRAWER**/
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -89,18 +91,20 @@ public class PantallaPrincipal extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        }
+         /* else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
         }
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /******/
 }
